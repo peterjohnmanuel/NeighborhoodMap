edf632f0dd4918452b3e02e19aec70842adaff20
Peter Manuel <peterjohn.manuel@gmail.com>
2016-08-04 11:19:11 +0200

    JS
    - Added javascript app.js.

---
+++ js/app.js
@@ -0,0 +1,10 @@
+/**
+ * Author : Peter John Manuel
+ * Date Created : 04/08/2016
+ * File: app.js
+ * Description : Holds relevant js functionality of the application.
+ */
+
+
+/** Global Variables */
+var map;
