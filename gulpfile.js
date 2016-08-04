8ed8587ff62327bd4dca04514549914c0c7e88a1
Peter Manuel <peterjohn.manuel@gmail.com>
2016-08-04 11:34:07 +0200

    Bootstrap
    - Installed and configured bootstrap sass.
    - Configured bootstrap sass gulp tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,20 @@
-var gulp = require('gulp')
+/** Gulp libraries */
 
+var gulp = require('gulp'),
+sass = require('gulp-sass');
 
-gulp.task('default', []);
+/** Configuration files */
+var config = {
+    bootstrap : './bower_components/bootstrap-sass'
+}
+
+/** Gulp Tasks */
+
+/** Task to convert sass to css */
+gulp.task('css', function(){ 
+    gulp.src('./scss/*.scss')
+    .pipe(sass({ includePaths: [config.bootstrap + '/assets/stylesheets']}))
+    .pipe(gulp.dest('css'));   
+});
+
+gulp.task('default', ['css']);
