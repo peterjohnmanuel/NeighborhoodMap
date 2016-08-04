e32a4f515d45f1d91488b5ccd8d88257f58234d4
Peter Manuel <peterjohn.manuel@gmail.com>
2016-08-04 12:15:58 +0200

    Package
    - Installed and configued jquery.
    - Include scripts on index.html
    - Include map css.
    - Added relevant font files.
    - Added init map function.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+'use strict';
 /**
  * Author : Peter John Manuel
  * Date Created : 04/08/2016
@@ -8,3 +9,18 @@
 
 /** Global Variables */
 var map;
+
+/** Functions */
+
+$(function () {
+
+    function initMap() {
+
+        map = new google.maps.Map( $('#map'), {
+            center: { lat: 40.7413549, lng: -73.9980244 },
+            zoom: 13
+        });
+
+    }
+
+});
---
+++ js/settings.js
@@ -0,0 +1,3 @@
+'use strict';
+
+var settings = { "GoogleAPIKey" : "AIzaSyBNxaV2Rfymk3kfsdWeU_ZK0VWg_-c5ZTg"}
