4dd5beea2cac8b718828835f86f7d7c87bd2a1da
Peter John Manuel <peterjohn.manuel@gmail.com>
2016-08-16 19:37:44 +0200

    Maps
    - Added maps.js for all google maps function.

--- js/app.js
+++ js/app.js
@@ -6,83 +6,8 @@
  * Description : Holds relevant js functionality of the application.
  */
 
-
 /** Global Variables */
-var map;
-var markers = [];
-
-
 var displayedPlaces = new PlaceViewModel();
-
-
-/** Functions */
-
-
-/**
- * Callback function used when initializing the map.
- * @func initMap
- */
-function initMap() {
-
-    ko.applyBindings(displayedPlaces);
-    displayedPlaces.initPlaces();
-
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: -34.2002806, lng: 18.3726442 },
-        zoom: 13,
-        mapTypeControl: false
-    });
-
-    InitializeMarkers();
-
-}
-
-/**
- * Create the markers for the locations.
- * @func InitializeMarkers
- */
-
-function InitializeMarkers(){
-
-        /** Creating a info windows */
-    var largerInfowindow = new google.maps.InfoWindow();
-    var bounds = new google.maps.LatLngBounds();
-
-
-    /**The following group uses the location array to create an array of markers on initialize.*/
-    for (var i = 0; i < displayedPlaces.places().length; i++) {
-
-        /** Get the position from the location array */
-        var position = displayedPlaces.places()[i].location;
-        var title = displayedPlaces.places()[i].title;
-
-        /**Create a marker per location, and put into markers array */
-        var marker = new google.maps.Marker({
-            map: map,
-            position: position,
-            title: title,
-            animation: google.maps.Animation.DROP,
-            id: i
-        });
-
-        /** Push the marker to our array of markers */
-        markers.push(marker);
-
-        /** Create an onclick event to open an infowindow at each marker */
-        marker.addListener('click', function () {
-            populateInfoWindow(this, largerInfowindow);
-        });
-
-        /** Extend the boundaries of the map for each marker */
-        bounds.extend(marker.position);
-
-        map.fitBounds(bounds);
-    }
-
-}
-
-
-
 
 $(function () {
 
